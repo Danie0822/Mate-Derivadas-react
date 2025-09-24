99662afe552f2ca363a890a5801b0f09604289f2
JuanPabloFloresDiaz <pablojuanfd@gmail.com>
2025-09-24 00:22:44 -0600

    ✨ Exportar componentes de modales: UserModal, DeleteUserModal, ExerciseModal, DeleteExerciseModal y ExerciseDetailModal

--- src/components/modals/index.js
+++ src/components/modals/index.js
@@ -1,2 +1,5 @@
 export { default as UserModal } from './UserModal';
 export { default as DeleteUserModal } from './DeleteUserModal';
+export { default as ExerciseModal } from './ExerciseModal';
+export { default as DeleteExerciseModal } from './DeleteExerciseModal';
+export { default as ExerciseDetailModal } from './ExerciseDetailModal';
